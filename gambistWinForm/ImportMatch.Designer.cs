bfb6009d86794311914ac1d7d3c520f229c96ce3
micram1901 <80063276+micram1901@users.noreply.github.com>
2021-06-17 17:21:42 +0400

    #initialisation export et import

---
+++ gambistWinForm/ImportMatch.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace gambistWinForm
+{
+    partial class ImportMatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.importButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gambist Client Lourd";
+            // 
+            // importButton
+            // 
+            this.importButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.importButton.Location = new System.Drawing.Point(15, 163);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(217, 42);
+            this.importButton.TabIndex = 2;
+            this.importButton.Text = "Importer un fichier de match";
+            this.importButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Importer un fichier de match";
+            // 
+            // ImportMatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.label1);
+            this.Name = "ImportMatch";
+            this.Text = "ImportMatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ gambistWinForm/Models/Pari.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gambistWinForm.Models
+{
+    public class Pari
+    {
+        public int Id { get; set; }
+        public int IdCategoriePari { get; set; }
+        public string Email { get; set; }
+        public decimal ValeurPari { get; set; }
+        public DateTime DatePari { get; set; }
+        public decimal TauxVictoire { get; set; }
+    }
+}
